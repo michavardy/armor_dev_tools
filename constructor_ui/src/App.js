ec23ca89077033b986bee1106c6913888ac608eb
michav <michavardy@gmail.com>
2021-08-29 15:37:37 +0300

    hello world

--- constructor_ui/src/App.js
+++ constructor_ui/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
-{/*import './App.css';*/}
+
 
 function App() {
   return (
     <div className="App">
-      <Counter />
-      <h1> test changes </h1>
+      {/*<Counter />*/}
+      <h1> hello world </h1>
     </div>
   );
 }
